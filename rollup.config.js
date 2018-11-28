0f59b80d0b3c87a288ee16c3eae923afdf2c9c6e
Stephane BARALE <stephane.barale@gmail.com>
2018-11-28 11:30:11 +0100

    Fix build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,189 @@
-import { uglify } from 'rollup-plugin-uglify'
+import { terser } from 'rollup-plugin-terser'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
 
-const env = process.env.NODE_ENV
-const config = {
-  input: 'src/index.js',
-  plugins: []
-}
+import pkg from './package.json'
 
-if (env === 'es' || env === 'cjs') {
-  config.output = { format: env }
-  config.plugins.push(babel())
-}
+export default [
+  // CommonJS
+  {
+    input: 'src/index.js',
+    output: { file: 'lib/react-redux-autoform.js', format: 'cjs', indent: false },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      nodeResolve({ extensions: ['.js', '.json', '.jsx', '.mjs'] }),
+      babel({ exclude: 'node_modules/**' }),
+      commonjs({
+        namedExports: {
+          'node_modules/prop-types/index.js': [
+            'arrayOf',
+            'bool',
+            'func',
+            'node',
+            'object',
+            'oneOf',
+            'oneOfType',
+            'shape',
+            'string'
+          ]
+        }
+      })
+    ]
+  },
 
-if (env === 'development' || env === 'production') {
-  config.output = { format: 'umd', name: 'ReactReduxAutoform' }
-  config.plugins.push(
-    nodeResolve({ jsnext: true }),
-    babel({ exclude: 'node_modules/**' }),
-    commonjs({
-      include: 'node_modules/**',
-      namedExports: {}
-    }),
-    replace({ 'process.env.NODE_ENV': JSON.stringify(env) })
-  )
-}
+  // ES
+  {
+    input: 'src/index.js',
+    output: { file: 'es/react-redux-autoform.js', format: 'es', indent: false },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      nodeResolve({ extensions: ['.js', '.json', '.jsx', '.mjs'] }),
+      babel({ exclude: 'node_modules/**' }),
+      commonjs({
+        namedExports: {
+          'node_modules/prop-types/index.js': [
+            'arrayOf',
+            'bool',
+            'func',
+            'node',
+            'object',
+            'oneOf',
+            'oneOfType',
+            'shape',
+            'string'
+          ]
+        }
+      })
+    ]
+  },
 
-if (env === 'production') {
-  config.plugins.push(
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false
-      }
-    })
-  )
-}
+  // ES for Browsers
+  {
+    input: 'src/index.js',
+    output: { file: 'es/react-redux-autoform.mjs', format: 'es', indent: false },
+    plugins: [
+      nodeResolve({ extensions: ['.js', '.json', '.jsx', '.mjs'] }),
+      babel(),
+      commonjs({
+        namedExports: {
+          'node_modules/prop-types/index.js': [
+            'arrayOf',
+            'bool',
+            'func',
+            'node',
+            'object',
+            'oneOf',
+            'oneOfType',
+            'shape',
+            'string'
+          ],
+          'node_modules/react/index.js': [
+            'Children',
+            'Component',
+            'createElement'
+          ],
+          'node_modules/react-redux/node_modules/react-is/index.js': ['isValidElementType']
+        }
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+      })
+    ]
+  },
 
-export default config
+  // UMD Development
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/react-redux-autoform.js',
+      format: 'umd',
+      name: 'ReactReduxAutoform',
+      indent: false
+    },
+    plugins: [
+      nodeResolve({ extensions: ['.js', '.json', '.jsx', '.mjs'] }),
+      babel(),
+      commonjs({
+        namedExports: {
+          'node_modules/prop-types/index.js': [
+            'arrayOf',
+            'bool',
+            'func',
+            'node',
+            'object',
+            'oneOf',
+            'oneOfType',
+            'shape',
+            'string'
+          ],
+          'node_modules/react/index.js': [
+            'Children',
+            'Component',
+            'createElement'
+          ],
+          'node_modules/react-redux/node_modules/react-is/index.js': ['isValidElementType']
+        }
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') })
+    ]
+  },
+
+  // UMD Production
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/react-redux-autoform.min.js',
+      format: 'umd',
+      name: 'ReactReduxAutoform',
+      indent: false
+    },
+    plugins: [
+      nodeResolve({ extensions: ['.js', '.json', '.jsx', '.mjs'] }),
+      babel(),
+      commonjs({
+        namedExports: {
+          'node_modules/prop-types/index.js': [
+            'arrayOf',
+            'bool',
+            'func',
+            'node',
+            'object',
+            'oneOf',
+            'oneOfType',
+            'shape',
+            'string'
+          ],
+          'node_modules/react/index.js': [
+            'Children',
+            'Component',
+            'createElement'
+          ],
+          'node_modules/react-redux/node_modules/react-is/index.js': ['isValidElementType']
+        }
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+      })
+    ]
+  }
+]
